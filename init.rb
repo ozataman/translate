091a2ffbae562b43fa151e937784f6e7c4e217b0
Ozgun Ataman <ozataman@gmail.com>
2009-01-23 09:20:17 -0600

    Require ya2yaml in the init file

--- init.rb
+++ init.rb
@@ -1,5 +1,12 @@
 require 'translate'
 
+begin
+  require 'ya2yaml'
+rescue => e
+  puts "ya2yaml gem not present. This gem is recommended so you get pretty UTF8 output in your locale YAML files.\n"
+  puts e
+end
+
 # TODO: Use new method available_locales once Rails is upgraded, see:
 # http://github.com/svenfuchs/i18n/commit/411f8fe7c8f3f89e9b6b921fa62ed66cb92f3af4
 def I18n.valid_locales
